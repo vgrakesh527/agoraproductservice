f21def2fe6d5a24184b2d4538fab2ec4eb327a63
nidhin.tt <nidhin@excelledia.com>
2024-12-27 14:45:52 +0530

    Initial application setup

---
+++ src/main/java/com/agora/agoraproductservice/AgoraProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agora.agoraproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraProductServiceApplication.class, args);
+	}
+
+}
